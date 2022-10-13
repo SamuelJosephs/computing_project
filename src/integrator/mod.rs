aa341038fa680ee21f23df3c3145b86c8e77fdb2
Samuel Josephs <smdjosephs@gmail.com>
2022-10-13 16:22:59 +0100

    Meant to commit this last time

--- src/integrator/mod.rs
+++ src/integrator/mod.rs
@@ -7,50 +7,80 @@ pub mod integrator {
 
     use crate::{Object::object::Ob, vec3::vec3d::{IsVec3d, Vec3d}};
 
-    pub fn euler_step<T,V>(input: Vec<Ob<T,V>>, dt: T, epsilon: T, a_matrix: &mut Vec<Vec<V>>,g: T) -> (Vec<Ob<T,V>>)
+    pub fn euler_step<T,V>(input: &mut Vec<Ob<T,V>>, dt: T, epsilon: T, a_matrix: &mut Vec<Vec<V>>,g: T) -> (Vec<Ob<T,V>>)
     where 
         T: real::Real,
         V: IsVec3d<Component = T>
     {
-        // input and a_matrix[0] must have the same length
-        // Set up Rc pointers to input as two owners are needed for each part of the for loops
-        let mut input1 = Rc::new(input);
-        let mut input2 = Rc::clone(&input1);
-        let input1_temp = Rc::get_mut(&mut input1).unwrap();
-        let input2_temp = Rc::get_mut(&mut input2).unwrap();
-        // Iterate through the input objects and memoize
-        // Calculate the 1/r^2 part of acceleration 
-        let size = input1_temp.len();
-        for (n,i) in input1_temp.iter_mut().enumerate() {
-            if n >= ((size / 2) + 1) { // Only need to go through half of the array
-                break;
-            } 
-            for (n2, j) in input2_temp.iter().enumerate() {
-                if n == n2 {
+        // Step through and calculate accelerations, then step through and update velocities then positions
+        for i in 0..(input.len()/2) { // Only need to go through the first half of the array
+            
+            for j in 0..input.len(){
+                if i == j {
                     continue;
                 }
-                let displacement = i.pos.sub(&j.pos);
-                let mut mag = displacement.mag_squared() + epsilon*epsilon;
-                mag = mag.powi(3);
-                mag = mag.sqrt();
-                mag = mag.powi(-1); // mag is now the denominator
-                a_matrix[n][n2] = displacement.scalarmul(mag);
-                a_matrix[n2][n] = a_matrix[n][n2].scalarmul(T::from(-1).unwrap());
+                
+                let displacement = input[j].pos.sub(&input[i].pos);
+                let denominator = displacement.mag_squared() + epsilon*epsilon
+                                    .powi(3).sqrt();
+                a_matrix[i][j] = a_matrix[i][j].add(&displacement.scalardiv(denominator)) ;
+                a_matrix[j][i] = a_matrix[i][j].scalarmul(T::from(-1).unwrap());
+
 
             }
-
-
         }
-
-        // Now go about updating velocity and position
-        for (n,i) in input1_temp.iter_mut().enumerate() {
-            for (n2,j) in input2_temp.iter_mut().enumerate() {
-                i.vel = i.vel.add(&a_matrix[n][n2].scalarmul(j.mass).scalarmul(g).scalarmul(dt));
+    
+        // Now we have 1/r^2 r hat in a_matrix we can work out accelerations
+        for i in 0..input.len() {
+            for j in 0..input.len() {
+                if i == j{
+                    continue;
+                }
+                input[i].vel = input[i].vel.add(
+                    &a_matrix[i][j].scalarmul(dt)
+                );
             }
-            i.pos = i.pos.add(&i.vel.scalarmul(dt));
+            input[i].pos = input[i].pos.add(
+                &input[i].vel.scalarmul(dt)
+            )
         }
 
-        let to_return = input1_temp.to_vec();
-        return to_return;
+        return input.to_vec();
     }
+
+
+    pub fn euler_step2<T,V>(mut input:  Vec<Ob<T,V>>, dt: T, epsilon: T, a_matrix: &mut Vec<Vec<V>>,g: T) -> (Vec<Ob<T,V>>)
+    where 
+        T: real::Real,
+        V: IsVec3d<Component = T> + Default
+    {
+        // Step through and calculate accelerations, then step through and update velocities then positions
+        for i in 0..input.len(){
+            let mut acceleration = V::default();
+            for j in 0..input.len(){
+                if i == j {continue};
+                
+                let r = input[j].pos.sub(
+                    &input[i].pos
+                );
+                let denominator = (r.mag_squared() + epsilon*epsilon).powi(3).sqrt();
+                acceleration = acceleration.add(&r.scalardiv(denominator).scalarmul(g).scalarmul(input[j].mass));
+            }
+            input[i].acc = acceleration;
+            
+
+        }
+
+        for i in 0..input.len(){
+            input[i].vel = input[i].vel.add(&input[i].acc.scalarmul(dt));
+            input[i].pos = input[i].pos.add(&input[i].vel.scalarmul(dt));
+        }
+        return input;
+    }
+
+
+
+    
+
+
 }
